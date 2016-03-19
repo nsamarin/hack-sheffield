cc25a22e8e7511181dcc1417245a1ad5fa792409
TheShadowOfChernobyl <sanja2659@rambler.ru>
2016-03-19 19:52:46 +0000

    Made temp sensor work

--- sketch_mar19a/sketch_mar19a.ino
+++ sketch_mar19a/sketch_mar19a.ino
@@ -2,6 +2,10 @@
 #include "rgb_lcd.h"
 
 rgb_lcd LCD;
+const int B=4275;                 // B value of the thermistor
+const int R0 = 100000;            // R0 = 100k
+const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A5
+ 
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,7 +13,16 @@ void setup() {
   LCD.begin(16, 2); 
 }
 
-  
+float gettemp(void){
+  int a = analogRead(pinTempSensor );
+ 
+    float R = 1023.0/((float)a)-1.0;
+    R = 100000.0*R;
+ 
+    float temperature=1.0/(log(R/100000.0)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
+
+    return temperature;
+}
 
 
 void loop() {
@@ -18,11 +31,9 @@ void loop() {
   // Check for button press
   if (digitalRead(8)) {
     LCD.setRGB(255, 255, 255);
+    LCD.print("Temperature = ");
+    LCD.print(gettemp());
     delay(1000);
-    LCD.print("Hello, World!");
-  }else{
-    LCD.setRGB(0, 0, 0); 
   }
-  
-
+  LCD.clear();
 }
