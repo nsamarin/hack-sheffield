27536d6d30563fffc095c3c5cacbf3c116d7d616
Nikita Samarin <n.samarin.s@gmail.com>
2016-03-19 19:03:26 +0000

    First commit

---
+++ sketch_mar19a/sketch_mar19a.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(8, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Check for button press
+  if (digitalRead(8)) {
+    
+  }
+  
+
+}
