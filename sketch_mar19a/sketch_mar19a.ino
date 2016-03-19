0215aa3692996fc3b560cd307ee707dd23d12028
TheShadowOfChernobyl <sanja2659@rambler.ru>
2016-03-19 22:14:06 +0000

    Commit for Nekit to code

--- sketch_mar19a/sketch_mar19a.ino
+++ sketch_mar19a/sketch_mar19a.ino
@@ -1,10 +1,13 @@
 #include <Wire.h>
+#include <Math.h>
 #include "rgb_lcd.h"
 
 rgb_lcd LCD;
 const int B=4275;                 // B value of the thermistor
 const int R0 = 100000;            // R0 = 100k
 const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A5
+
+enum drinktype {COLD, ROOMTEMP, HOT};
  
 
 void setup() {
@@ -69,13 +72,72 @@ void checkForStabilize() {
 
 }
 
+unsigned char getDrinkType(){
+  unsigned char type = 0;
+  float temp = getTemp();
+  if (temp < 15){
+    type = COLD;
+  }else if (temp > 37){
+    type = HOT;
+  }else{
+    type = ROOMTEMP;
+  }
+  return type;
+}
+
+void displayTimeTillCooldown(float optimal) {
+
+  // define variables
+  float previousTemp = -1;
+  float currentTemp = -1;
+  float timeTillCool;
+  float rateOfChange;
+
+  // loop until optimal temperature is reached
+  while (true) {
+    if (currentTemp == -1) {
+      previousTemp = getTemp();
+      currentTemp = previousTemp;
+      //LCD.clear();
+      //LCD.print("topkek");
+    } else {
+      currentTemp = getTemp();
+      // get rate of change of temperature per 1 second
+      rateOfChange = abs((previousTemp - currentTemp)/2.5);
+   
+      if (!rateOfChange){
+        continue;
+      }
+      
+      timeTillCool = abs(currentTemp - optimal) / rateOfChange;
+
+      LCD.clear();
+      LCD.print("Time till cooldown:");
+      LCD.setCursor(0, 1);
+      LCD.print(timeTillCool);
+      LCD.print(" seconds");
+
+      if (timeTillCool < 0.1) return;
+      previousTemp = currentTemp;
+      delay(1500);
+      
+    }
+  }
+}
 void loop() {
   
   while (digitalRead(8)){
     LCD.setRGB(255, 255, 255);
-    checkForStabilize();
+    //checkForStabilize();
     
+    LCD.clear();
+    LCD.print("kek");
+    delay(1000);
+    
+    displayTimeTillCooldown(23);
   }
+
+  LCD.clear();
   /*
   // put your main code here, to run repeatedly:
 
