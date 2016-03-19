4b1764a11ac99110a6d2d0d343d4cc349855ad20
TheShadowOfChernobyl <sanja2659@rambler.ru>
2016-03-19 19:40:13 +0000

    LCD Lib works

--- sketch_mar19a/sketch_mar19a.ino
+++ sketch_mar19a/sketch_mar19a.ino
@@ -1,20 +1,27 @@
 #include <Wire.h>
-#include <"rgb_lcd.h">
+#include "rgb_lcd.h"
 
+rgb_lcd LCD;
 
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin();
   pinMode(8, INPUT);
-
+  LCD.begin(16, 2); 
 }
+
+  
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   // Check for button press
   if (digitalRead(8)) {
-    
+    LCD.setRGB(255, 255, 255);
+    delay(1000);
+    LCD.print("Hello, World!");
+  }else{
+    LCD.setRGB(0, 0, 0); 
   }
   
 
